2db6362f766541850c9d5eeb2fc0d5dc118f8314
Rehmatullakhan <57626325+Rehmatullakhan@users.noreply.github.com>
2025-08-11 12:03:11 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -17,8 +17,57 @@ const winPatternsArray = [
     [0,4,8],
 ];
 
+const showWinner = (winner) =>{
+  msg.append(winner);
+};
+
+const disabledBtn = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    };
+};
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    };
+};
+
+const checkWinner = () =>{
+   
+     for(let pattern of winPatternsArray){
+     let val1 = boxes[pattern[0]].innerText;
+     let val2 = boxes[pattern[1]].innerText;
+     let val3 = boxes[pattern[2]].innerText;
+     
+     if(val1 !== ""  && val2 !== "" && val3 !== "" ){
+        if(val1 === val2 && val2 === val3){
+           console.log("winner" ,val1);
+        
+          // console.log(moves);
+            showWinner(val1);
+            disabledBtn();
+        };
+
+     };
+
+    //  if(moves==9){
+    //     console.log("match is drawn");
+    //}
+     
+    
+};
+
+
+
+};
+
 boxes.forEach((box) => {
+    move = 0;
     box.addEventListener("click", ()=>{
+        move ++;
+        console.log(move);
         console.log("box was clicked");
         if(turnO){
                box.innerText = "O";
@@ -30,55 +79,27 @@ boxes.forEach((box) => {
         }
          box.disabled = true;
          checkWinner();
+         if(move==9){
+            msg.innerText = "Match Drawn";
+            move = 0;
+         }
     });
 });
 
-const showWinner = (winner) =>{
-    msg.innerText = `Congratullations  winner is ${winner}`;
-}
-
-const disabledBtn = () => {
-    for(let box of boxes){
-        box.disabled = true;
-    }
-}
 
 
-const checkWinner = () =>{
-     for(let pattern of winPatternsArray){
-     let val1 = boxes[pattern[0]].innerText;
-     let val2 = boxes[pattern[1]].innerText;
-     let val3 = boxes[pattern[2]].innerText;
-     
-     if(val1 !== ""  && val2 !== "" && val3 !== "" ){
-        if(val1 === val2 && val2 === val3){
-            console.log("winner" ,val2);
-            showWinner(val1);
-            disabledBtn();
-        }
-
-     }
-}}
-
-
-// const resetGamef = () => {
-//     turnO = true;
-//     enableBoxes();
-// };
-const enableBoxes = () => {
-    for(let box of boxes){
-        box.disabled = false;
-        box.innerText = "";
-    }
-};
 
 newGameBtn.addEventListener("click",() => {
+    move = 0;
     turnO =true;
     enableBoxes();
-    
+    msg.innerText= "Winner: ";
 });
 
 resetBtn.addEventListener("click", () => {
+    move = 0;
     turnO= true;
+    msg.innerText="Winner: ";
     enableBoxes();
+
 });
